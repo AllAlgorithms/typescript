45eed96e0f66c6a27086ec5256f16dd531a0a302
Anton Cataldi <antoncataldi@gmail.com>
2018-10-22 23:45:22 -0400

    refactor quickSort to class syntax

--- sorting/quicksort/quickSort.ts
+++ sorting/quicksort/quickSort.ts
@@ -1,31 +1,37 @@
 
-//implement quicksort using Lomuto partition scheme
-export function quickSort(arr: Array<number>, low: number, high: number): void {
-    if(low < high){
-        const pivot = partition(arr, low, high);
-        quickSort(arr, low, pivot);
-        quickSort(arr, pivot+1, high);
+// implement quicksort using Lomuto partition scheme
+export class Sorter {
+
+    public quickSort (arr: Array<number>, low: number, high: number): Array<number> {
+        let bottomSorted, topSorted
+        if(low < high){
+            const pivot = this.partition(arr, low, high);
+            bottomSorted = this.quickSort(arr, low, pivot);
+            topSorted = this.quickSort(arr, pivot+1, high);
+        }
+        return [...bottomSorted, ...topSorted];
     }
 
-}
-
-function partition(arr: Array<number>, low: number, high: number): number {
-    const pivot = arr[high];
-    let i, j = low   
-    while( j < high){
-        if(arr[j] < pivot){
-            if(i !== j){
-                let temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+    private partition(arr: Array<number>, low: number, high: number): number {
+        const pivot = arr[high];
+        let i, j = low   
+        while( j < high){
+            if(arr[j] < pivot){
+                if(i !== j){
+                    let temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                i++;
             }
-            i++;
-        }
-
-    } 
-    let temp = arr[i];
-    arr[i] = arr[high];
-    arr[high] = temp;
-    return i;
-
+            j++;
+        } 
+        let temp = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp;
+        return i;
+    
+    }
 }
+
+
