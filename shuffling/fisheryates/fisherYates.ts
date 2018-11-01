da3ec0e2522488d3d7994370dc2a9d70fcff6656
Anton Cataldi <antoncataldi@gmail.com>
2018-10-31 23:37:13 -0400

    fix index

--- shuffling/fisheryates/fisherYates.ts
+++ shuffling/fisheryates/fisherYates.ts
@@ -1,7 +1,6 @@
 export function fisherYates(arr: Array<number>): void {
-    // for i from n−1 downto 1 do
-    for(let i = arr.length - 1; i > 0; i++){
-        const j = Math.floor(Math.random()* i );
+    for(let i = arr.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * i );
         const temp = arr[j];
         arr[j] = arr[i];
         arr[i] = temp;
