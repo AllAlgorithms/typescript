ff31426a14ce1b7d1b6bedd89fb40a4781224bee
Abraham Hernandez <abraham@abranhe.com>
2021-10-05 21:22:35 -0400

    format

--- algorithms/math/factorial/factorial_iterative.ts
+++ algorithms/math/factorial/factorial_iterative.ts
@@ -4,11 +4,11 @@
  * @source https://github.com/AllAlgorithms/TypeScript
  */
 export const factorial = (n: number): number => {
-    let ret = 1;
+  let ret = 1;
 
-    for (let i = 2; i <= n; i++) {
-        ret *= i;
-    }
+  for (let i = 2; i <= n; i++) {
+    ret *= i;
+  }
 
-    return ret;
+  return ret;
 };
